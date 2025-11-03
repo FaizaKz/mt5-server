0f4e8cb6ac921ae03a13460971054e23a5f85505
FaizaKz <faizakz1987@gmail.com>
2025-11-03 08:51:39 +0500

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -1,10 +1,18 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(400).json({ error: "POST only" });
   }
 
+  console.log("Received body:", req.body);
+
   const msg = req.body.message || "No message";
-  console.log("MT5 Message:", msg);
 
   return res.status(200).json({ status: "OK", received: msg });
 }
+
